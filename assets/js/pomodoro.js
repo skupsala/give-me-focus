20ce899f0b4537a992cece958c40da449149eb63
Samuli Kupsala <samuli.kupsala@gmail.com>
2018-11-10 11:46:51 +0200

    Enable short breaks and long breaks

--- assets/js/pomodoro.js
+++ assets/js/pomodoro.js
@@ -1,52 +1,172 @@
 (function () {
-  var POMODORO = 1 * 2 * 1000;
+
+  var POMODORO_MINUTES = 0.1;
+  var SHORT_BREAK_MINUTES = 0.05;
+  var LONG_BREAK_MINUTES = 0.075;
+
+  var PHASE_DURATIONS = {
+    'POMODORO': POMODORO_MINUTES * 60 * 1000,
+    'SHORT_BREAK': SHORT_BREAK_MINUTES * 60 * 1000,
+    'LONG_BREAK': LONG_BREAK_MINUTES * 60 * 1000,
+  };
+
+  var currentPhaseIndex = 0;
+  var PHASE_ROTATION = [
+    'POMODORO',
+    'SHORT_BREAK',
+    'POMODORO',
+    'SHORT_BREAK',
+    'POMODORO',
+    'SHORT_BREAK',
+    'POMODORO',
+    'LONG_BREAK'
+  ];
 
   var _startTime = null;
   var _interval = null;
+  var _timerElem = document.getElementById('timer');
+  var _focusTextInputElem = document.getElementById('focus-text-input');
+  var _focusTextValidationErrorElem = document.getElementById('focus-text-input-validation-error');
+  var _focusTextElem = document.getElementById('focus-text');
+  var _startPomodoroButton = document.getElementById('start-pomodoro');
 
   function formatTimer(milliseconds) {
     var remainingSeconds = Math.round(milliseconds / 1000);
     var minutes = Math.floor(remainingSeconds / 60);
     var seconds = remainingSeconds - minutes * 60;
-    return minutes + ' : ' + seconds;
+    return (minutes < 10 ? '0' : '') + minutes + ' : ' + (seconds < 10 ? '0' : '') + seconds;
   }
 
   function startTimer() {
-    _startTime = Date.now();
-    var focusTextElem = document.getElementById('focus-text');
-    var text = focusTextElem.value;
-    focusTextElem.style.display = 'none';
-    var focusBlockquote = document.getElementById('focus-blockquote');
-    focusBlockquote.innerText = text;
-    focusBlockquote.style.display = 'block';
-    _interval = setInterval(function () {
-      document.getElementById('timer').innerText = formatTimer(POMODORO - (Date.now() - _startTime));
+    _startPomodoroButton.classList.add('disabled');;
 
-      if (_startTime + POMODORO < Date.now()) {
+    _startTime = Date.now();
+
+    if (getCurrentPhase() === 'POMODORO') {
+      lockFocusField();
+    }
+
+    _interval = setInterval(function () {
+      var duration = PHASE_DURATIONS[getCurrentPhase()];
+      _timerElem.innerText = formatTimer(duration - (Date.now() - _startTime));
+      if (_startTime + duration < Date.now()) {
         resetTimer();
       }
-    }, 1000);
+    }, 500);
   }
 
   function resetTimer() {
+    _startPomodoroButton.classList.remove('disabled');
     if (_interval !== null) {
       clearInterval(_interval);
       _interval = null;
-      var focusTextElem = document.getElementById('focus-text');
-      focusTextElem.style.display = 'block';
-      var focusBlockquote = document.getElementById('focus-blockquote');
-      focusBlockquote.style.display = 'none';
-      // document.getElementById('timer').innerText = formatTimer(POMODORO);
-      alert('Valmis!');
+    }
+
+    if (getCurrentPhase() === 'POMODORO') {
+      enableFocusField();
+    }
+
+    formatTimer(0);
+  }
+
+  function lockFocusField() {
+    document.getElementById('focus-text-container').style.display = 'block';
+    var text = _focusTextInputElem.value;
+    _focusTextInputElem.style.display = 'none';
+    _focusTextElem.innerText = text;
+    _focusTextElem.style.display = 'block';
+    _focusTextValidationErrorElem.style.display = 'none';
+
+  }
+
+  function enableFocusField() {
+    document.getElementById('focus-text-container').style.display = 'block';
+    _focusTextInputElem.style.display = 'block';
+    _focusTextInputElem.focus();
+    _focusTextInputElem.select();
+    _focusTextElem.style.display = 'none';
+    _focusTextValidationErrorElem.style.display = 'block';
+
+  }
+
+  function getCurrentPhase() {
+    return PHASE_ROTATION[currentPhaseIndex];
+  }
+
+  function setPhase(newPhase) {
+    // Roll to next phase
+    // currentPhaseIndex = currentPhaseIndex >= PHASE_ROTATION.length - 1 ? 0 : currentPhaseIndex + 1;
+
+    // TODO rotate next occurence of phases - not first
+    currentPhaseIndex = PHASE_ROTATION.indexOf(newPhase);
+    var duration = PHASE_DURATIONS[getCurrentPhase()];
+    _timerElem.innerText = formatTimer(duration);
+
+    switch (getCurrentPhase()) {
+      case 'POMODORO':
+        enableFocusField();
+        break;
+      case 'SHORT_BREAK':
+        document.getElementById('focus-text-container').style.display = 'none';
+        break;
+      case 'LONG_BREAK':
+        document.getElementById('focus-text-container').style.display = 'none';
+        break;
+    }
+  }
+
+
+  function validateFocusText() {
+    var text = _focusTextInputElem.value.trim();
+    if (text) {
+      _focusTextValidationErrorElem.style.visibility = 'hidden';
+      _startPomodoroButton.classList.remove('disabled');
+      return true;
+    } else {
+      _startPomodoroButton.classList.add('disabled');
+      _focusTextValidationErrorElem.style.visibility = 'visible';
+      return false;
     }
   }
 
   function init() {
-    document.getElementById('timer').innerText = formatTimer(POMODORO);
-    document.getElementById('start-pomodoro').addEventListener('click', function (evt) {
+    var duration = PHASE_DURATIONS[getCurrentPhase()];
+    _timerElem.innerText = formatTimer(duration);
+    _startPomodoroButton.addEventListener('click', function (evt) {
       evt.preventDefault();
-      startTimer();
+      if (getCurrentPhase() === 'POMODORO') {
+        if (validateFocusText()) {
+          startTimer();
+        }
+      } else {
+        startTimer();
+      }
     });
+    _focusTextInputElem.oninput = function (evt) {
+      validateFocusText();
+    };
+
+    document.getElementById('pomodoro').addEventListener('click', function () {
+      setPhase('POMODORO');
+      resetTimer();
+      document.querySelector('#phase-container .active').classList.remove('active');
+      document.querySelector('#phase-container #pomodoro').classList.add('active');
+    });
+
+    document.getElementById('short-break').addEventListener('click', function () {
+      setPhase('SHORT_BREAK');
+      resetTimer();
+      document.querySelector('#phase-container .active').classList.remove('active');
+      document.querySelector('#phase-container #short-break').classList.add('active');
+    });
+
+    document.getElementById('long-break').addEventListener('click', function () {
+      setPhase('LONG_BREAK');
+      resetTimer();
+      document.querySelector('#phase-container .active').classList.remove('active');
+      document.querySelector('#phase-container #long-break').classList.add('active');
+    });
+
   }
 
   init();
